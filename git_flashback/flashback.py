87e44ae5afd648e8c3e71845f951095507881b95
yohan-pg <pg.yohan@gmail.com>
2022-05-19 19:08:46 -0400

    added some more tests

--- git_flashback/flashback.py
+++ git_flashback/flashback.py
@@ -7,13 +7,20 @@ from .gitimport import add_repository_to_path, GitImporter
 import sys
 
 
+class InvalidTimestampOrRef(Exception):
+    pass
+
+
 def flashback(timestamp_or_ref: str):
+    assert isinstance(timestamp_or_ref, str)
+    
     timestamp_or_ref = timestamp_or_ref.split("/")[-1]
 
     if git_head_matches_timestamp(timestamp_or_ref):
         return dummy_context_manager()
     else:
         return load_modules_from_git(timestamp_or_ref)
+        
 
 
 @contextlib.contextmanager
@@ -26,8 +33,11 @@ def load_modules_from_git(rev="HEAD", repo=".", in_repo_path=""):
     current_modules = sys.modules.copy()
     sys.modules.clear()
     sys.modules.update(init_modules)
-    path = add_repository_to_path(repo, rev, in_repo_path)
-    
+    try:
+        path = add_repository_to_path(repo, rev, in_repo_path)
+    except KeyError:
+        raise InvalidTimestampOrRef()
+
     final_modules = {}
     yield final_modules
     
--- tests/test_git_flashback.py
+++ tests/test_git_flashback.py
@@ -1,21 +1,26 @@
-from git_flashback import * 
+from git_flashback import *
 
 import pickle
 import sys
 import shutil
 import importlib
 import time
-import os 
+import os
+
+import pygit2
+
 
 class setup_tmp_dir:
     shutil.rmtree("tmp", ignore_errors=True)
     os.mkdir("tmp")
-    open("tmp/__init__.py", "w").close() #! This should not be required (bug)
+    open("tmp/__init__.py", "w").close()  #! This should not be required (bug)
     shutil.copy("tests/fixtures/before.py", "tmp/example.py")
 
 
 def _load_or_reload(module_path: str):
-    os.system("rm -r tmp/__pycache__") #! I don't understand why this is required, but it is
+    os.system(
+        "rm -r tmp/__pycache__"
+    )  #! I don't understand why this is required, but it is
     if module_path in sys.modules:
         del sys.modules[module_path]
     return importlib.import_module(module_path)
@@ -43,50 +48,59 @@ def test_restoration():
 
     module = _load_example_module_after_changes()
     assert module.Example().f() == 2
-    
+
     with flashback(timestamp):
         assert pickle.load(open(f"tmp/{timestamp}.pkl", "rb")).f() == 1
 
 
-def test_tags_made_in_the_same_second_are_shared():
-    pass
+def test_tags_made_in_the_same_second_are_shared_without_error():
+    timestamp1 = snapshot()
+    timestamp2 = snapshot()
+    assert timestamp1 == timestamp2
 
 
+def test_errors_are_throw_for_missing_timestamps():
+    try:
+        with flashback("00000000T000000"):
+            pass
+    except InvalidTimestampOrRef:
+        return
+    assert False
 
 
-def test_paths_are_ignored_in_timestamps():
-    """
-    For conveniance, flashback will ignore everything before the rightmost forward slash, so a path like "experiments/20220519T150016.264685" can be passed directly to it.
-    """
+def test_path_prefixes_are_removed_from_timestamps():
     timestamp = snapshot()
-
     with flashback("any_path/" + timestamp):
         pass
 
 
-def test_timestamp_matches():
-    """
-    When attempting to load from a timestamp which matches the current HEAD, snapshot will return the current tag (doing nothing) and flashback will instead load from the present (does nothing also).
-    """
+def test_flashback_works_with_commit_numbers():
+    repo = pygit2.Repository(".")
+    timestamp = snapshot()
+    commit_hash = repo.get(repo.revparse_single(timestamp).target).id.hex  # type: ignore
+    with flashback(commit_hash):
+        pass
 
 
-def test_code_editing():
-    pass
+# def test_timestamp_matches():
+#     """
+#     When attempting to load from a timestamp which matches the current HEAD, snapshot will return the current tag (doing nothing) and flashback will instead load from the present (does nothing also).
+#     """
 
 
-def test_flashback_to_commit():
-    pass
+# def test_code_editing():
+#     pass
 
 
 def test_isinstance():
     pass
 
+
 def test_issubclass():
     pass
 
 
-
 # - test: paths work when running from another directory
 # - test: torch compatibiltiy
-# - test: repo.index.add_all() reflects what the user adds manually to the index 
+# - test: repo.index.add_all() reflects what the user adds manually to the index
 #       (this will be tricky to test! maybe just do it manually)
